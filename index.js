7527402699ffaf337a3035cc4467f594a129f94f
syiv-ie <kerrsylvie@gmail.com>
2024-12-04 17:02:46 -0800

    soul

---
+++ index.js
@@ -0,0 +1,63 @@
+//(made by Sylvie) 🏳️‍⚧️
+//disord @syiv 
+
+let artCount = 1;
+let soul = document.getElementById("soul");
+
+
+fillSoul();
+function fillSoul(){
+    let i = 0;
+
+    findArt();
+}
+
+function findArt(){
+    var img = new Image();
+    img.src = "images/art/" + artCount + ".png";
+
+    img.onerror = function(){ // Failed to load. were done
+        console.log("uh")
+    };
+    img.onload = function(){ // Loaded successfully. prepend and look for another
+        let art = document.createElement("img");
+        art.className = "art";
+        art.src = "images/art/" + artCount + ".png";
+        soul.prepend(art);
+        artCount++
+        findArt();
+    };
+}
+
+
+
+/*
+
+    //console.log("hu")
+    let body = document.getElementById("body");
+    body.onload = function(){
+        console.log("hu")
+    }
+
+do{
+        i++;
+    
+        var f = new File("images/art/" + i + ".png", "file");
+        const f = new File([i], "images/art/" + i + ".png", {
+            type: "text/plain",
+          });
+    }while(f.exists());
+    
+    artCount = i--;
+    console.log(i);
+
+    async function loadContent(url) {
+        const exists = await fileExists("images/art/" + 1 + ".png");
+        if (exists) {
+            // Load content
+            console.log('Loading content...');
+        } else {
+            console.log('Content not available');
+        }
+    }
+*/
