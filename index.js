d4d01d9a7bb7feffa5bd3afe8cfe882749fa0b06
syiv-ie <kerrsylvie@gmail.com>
2024-12-27 14:20:43 -0800

    I added persona types and the first being friend codes

--- index.js
+++ index.js
@@ -3,27 +3,57 @@
 
 let artCount = 21;
 let soul = document.getElementById("soul");
+let fcs = [
+    'Steam: ',
+    'picori_blade',
+    'Epic Games: ',
+    'Syiv.ie',
+    'BNet: ',
+    'Syiv#1296',
+    'Switch: ',
+    'SW-8434-4775-3500',
+    'FEH: ',
+    '6417991149',
+]
 
+persona = window.location.search
+console.log(persona)
 
 fillSoul();
 function fillSoul(){
 
-    for(let i = 1; i <= artCount; i++){
-        let art = document.createElement("img");
-        art.className = "art";
-        art.src = "images/art/" + i + ".png";
-        soul.prepend(art);
+    if((persona).localeCompare('?fc') == 0){
+        let fcTitle = document.createElement("div");
+        fcTitle.id = "fcTitle";
+        soul.appendChild(fcTitle);
+        fcTitle.innerHTML = "Tap the box to copy!"
+        for(let i = 0; i < fcs.length; i = i + 2){
+            let fc = document.createElement("div");
+            fc.className = "fc";
+            soul.appendChild(fc);
+            fc.innerHTML = fcs[i] + fcs[i+1];
+            fc.onclick = function(){
+                navigator.clipboard.writeText(fcs[i+1]);
+            }
+        }
+    }else{
+        for(let i = 1; i <= artCount; i++){
+            let art = document.createElement("img");
+            art.className = "art";
+            art.src = "images/art/" + i + ".png";
+            soul.prepend(art);
+        }
     }
 
-    //findArt();
 }
 
 
 
-/*
+/* -----old code-----
 
 
 
+//findArt();
 function findArt(){
     var img = new Image();
     img.src = "images/art/" + artCount + ".png";
